cf7e408bcdee644ccbb6075f83265a470c22ed19
Piotr <xmestre@wp.pl>
2023-05-26 21:49:41 +0100

    binary tree remove method added

--- src/Easy/BST.java
+++ src/Easy/BST.java
@@ -51,6 +51,51 @@ public class BST {
             }
     }
 
+    public int getMinValue(){
+            if(left == null){
+                return this.value;
+            } else {
+                return left.getMinValue();
+            }
+    }
+
+    public BST remove(int value){
+            remove(value, null);
+            return this;
+    }
+
+    public void remove(int value, BST parent){
+            if(value < this.value){
+                if(left != null){
+                    left.remove(value, this);
+                }
+            } else if (value > this.value) {
+                if(right != null){
+                    right.remove(value, this);
+                }
+            } else {
+                if(left != null && right != null){
+                    this.value = right.getMinValue();
+                    right.remove(this.value, this);
+                } else if (parent == null) {
+                    if (left != null){
+                        this.value = left.value;
+                        right = left.right;
+                        left = left.left;
+                    } else if (right !=null) {
+                        this.value = right.value;
+                        left = right.left;
+                        right = right.right;
+                    } else {
+                        // This is a single-node tree; nothing is done.
+                    }
+                } else if (parent.left == this) {
+                    parent.left = left != null ? left : right;
+                }else if (parent.right == this){
+                    parent.right = left != null ? left : right;
+                }
+            }
+    }
 
 
 }
